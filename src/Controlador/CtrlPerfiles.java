4beca9759ad9e96c987c4e735d721a47f856975a
vladnn100 <frank_a.pardo_2017@hotmail.com>
2023-06-01 16:31:47 -0500

    Extrayendo metodos del metodo largo CtrlPerfiles

--- src/Controlador/CtrlPerfiles.java
+++ src/Controlador/CtrlPerfiles.java
@@ -49,229 +49,17 @@ public class CtrlPerfiles {
             }
         });
 
-        ////////////////
-        this.vista.btnAdministrarCuenta.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                vista.dispose();
-                FrmAdministrarCuenta fAdminCuenta = new FrmAdministrarCuenta();
-                CtrlAdministrarCuenta ctrlAdminCuenta = new CtrlAdministrarCuenta(modelo, fAdminCuenta);
-                ctrlAdminCuenta.setCorreo(auxCorreo);
-                ctrlAdminCuenta.init();
-            }
-        });
-        this.vista.btnEditar1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        this.vista.btnAdministrarCuenta.addActionListener(this.accionAdministrarCuenta());
+        this.vista.btnEditar1.addActionListener(this.accionEditarPerfil1());
+        this.vista.btnEditar2.addActionListener(this.accionEditarPerfil2());
+        this.vista.btnEditar3.addActionListener(this.accionEditarPerfil3());
+        this.vista.btnEditar4.addActionListener(this.accionEditarPerfil4());
+        this.vista.btnPerfil1.addActionListener(this.accionAccederPerfil1());
+        this.vista.btnPerfil2.addActionListener(this.accionAccederPerfil2());
+        this.vista.btnPerfil3.addActionListener(this.accionAccederPerfil3());
+        this.vista.btnPerfil4.addActionListener(this.accionAccederPerfil4());
+        this.vista.btnSalir.addActionListener(this.accionSalir());
 
-                vista.dispose();
-
-                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
-
-                fEditarPerfiles.indexPerfilAux = 0;
-
-                CtrlEditarPerfiles controlador = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
-                controlador.setCorreo(auxCorreo);
-                controlador.init();
-            }
-        });
-
-        this.vista.btnEditar2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                vista.dispose();
-
-                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
-
-                fEditarPerfiles.indexPerfilAux = 1;
-
-                CtrlEditarPerfiles controlador = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
-                controlador.setCorreo(auxCorreo);
-                controlador.init();
-            }
-        });
-
-        this.vista.btnEditar3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                vista.dispose();
-
-                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
-
-                fEditarPerfiles.indexPerfilAux = 2;
-
-                CtrlEditarPerfiles cEditarPerfiles = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
-                cEditarPerfiles.setCorreo(auxCorreo);
-                cEditarPerfiles.init();
-            }
-        });
-
-        this.vista.btnEditar4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                vista.dispose();
-
-                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
-
-                fEditarPerfiles.indexPerfilAux = 3;
-
-                CtrlEditarPerfiles controlador = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
-                controlador.setCorreo(auxCorreo);
-                controlador.init();
-            }
-        });
-
-        this.vista.btnSalir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                try {
-                    vista.dispose();
-                    Cuenta a = modelo.cuentaActiva(auxCorreo);
-                    String Correo = a.getUsuario().getCorreo();
-                    System.out.println(Correo);
-                    modelo.cuentaActiva(auxCorreo).getUsuario().disminuirDispositivo();
-                    // modelo.cambiarEstadoSesion(Correo);
-                    //GUARDANDO LOS CAMBIOS EN EL ARCHIVO//
-                    Archivo archivo = new Archivo();
-                    archivo.serializar(Archivo.archivoArregloCuentas, modelo);
-                    // // // // // // // // // // // // // // 
-
-                    FrmInicio fInicio = new FrmInicio();
-                    CtrlInicio ctrlInicio = new CtrlInicio(modelo, fInicio);
-
-                    ctrlInicio.init();
-                } catch (CustomException e) {
-                    System.out.println("EXCEPCION: " + e.getMessage());
-                }
-            }
-        });
-
-        this.vista.btnPerfil1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                try {
-                    try {
-                        Cuenta c = modelo.cuentaActiva(auxCorreo); // Se obtiene la cuenta activa
-
-                        if (c.debeCobrar()) { // Si la cuenta debe cobrar
-                            if (!c.cobrar()) { // Si no se puede cobrar
-                                JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
-                                return; // Se muestra un mensaje y se termina la ejecución
-                            }
-                        }
-                    } catch (CustomException ex) {
-                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    modelo.cuentaActiva(auxCorreo).getPerfil(0).setActivo(true);
-                    vista.dispose();
-                    FrmCatalogo fCatalogo = new FrmCatalogo();
-                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
-                    ctrlCatalogo.setCorreo(auxCorreo);
-                    ctrlCatalogo.init();
-                } catch (CustomException e) {
-                    JOptionPane.showMessageDialog(vista, e.getMessage());
-                }
-            }
-        });
-
-        this.vista.btnPerfil2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                try {
-
-                    Cuenta c = null;
-
-                    try {
-                        c = modelo.cuentaActiva(auxCorreo);
-                    } catch (CustomException ex) {
-                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    if (c.debeCobrar()) {
-                        if (!c.cobrar()) {
-                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
-                            return;
-                        }
-                    }
-
-                    modelo.cuentaActiva(auxCorreo).getPerfil(1).setActivo(true);
-                    vista.dispose();
-                    FrmCatalogo fCatalogo = new FrmCatalogo();
-                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
-                    ctrlCatalogo.setCorreo(auxCorreo);
-                    ctrlCatalogo.init();
-                } catch (CustomException e) {
-                    JOptionPane.showMessageDialog(vista, e.getMessage());
-                }
-            }
-        });
-
-        this.vista.btnPerfil3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                try {
-
-                    Cuenta c = null;
-
-                    try {
-                        c = modelo.cuentaActiva(auxCorreo);
-                    } catch (CustomException ex) {
-                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    if (c.debeCobrar()) {
-                        if (!c.cobrar()) {
-                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
-                            return;
-                        }
-                    }
-
-                    modelo.cuentaActiva(auxCorreo).getPerfil(3).setActivo(true);
-                    vista.dispose();
-                    FrmCatalogo fCatalogo = new FrmCatalogo();
-                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
-                    ctrlCatalogo.setCorreo(auxCorreo);
-                    ctrlCatalogo.init();
-                } catch (CustomException e) {
-                    JOptionPane.showMessageDialog(vista, e.getMessage());
-                }
-            }
-        });
-
-        this.vista.btnPerfil4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                try {
-
-                    Cuenta c = null;
-
-                    try {
-                        c = modelo.cuentaActiva(auxCorreo);
-                    } catch (CustomException ex) {
-                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    if (c.debeCobrar()) {
-                        if (!c.cobrar()) {
-                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
-                            return;
-                        }
-                    }
-
-                    modelo.cuentaActiva(auxCorreo).getPerfil(3).setActivo(true);
-                    vista.dispose();
-                    FrmCatalogo fCatalogo = new FrmCatalogo();
-                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
-                    ctrlCatalogo.setCorreo(auxCorreo);
-                    ctrlCatalogo.init();
-                } catch (CustomException e) {
-                    JOptionPane.showMessageDialog(vista, e.getMessage());
-                }
-            }
-        });
     }
 
     public void init() {
@@ -303,6 +91,205 @@ public class CtrlPerfiles {
 
     public void setCorreo(String Correo) {
         this.auxCorreo = Correo;
+    }
+
+    private ActionListener accionAdministrarCuenta(){
+            return (ActionEvent ae) -> {
+                vista.dispose();
+                FrmAdministrarCuenta fAdminCuenta = new FrmAdministrarCuenta();
+                CtrlAdministrarCuenta ctrlAdminCuenta = new CtrlAdministrarCuenta(modelo, fAdminCuenta);
+                ctrlAdminCuenta.setCorreo(auxCorreo);
+                ctrlAdminCuenta.init();
+            };
+    }
+
+    private ActionListener accionEditarPerfil1(){
+            return (ActionEvent ae) -> {
+                vista.dispose();
+                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
+                fEditarPerfiles.indexPerfilAux = 0;
+                CtrlEditarPerfiles cEditarPerfiles = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
+                cEditarPerfiles.setCorreo(auxCorreo);
+                cEditarPerfiles.init();
+            };
+    }
+
+    private ActionListener accionEditarPerfil2(){
+            return (ActionEvent ae) -> {
+                vista.dispose();
+                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
+                fEditarPerfiles.indexPerfilAux = 1;
+                CtrlEditarPerfiles controlador = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
+                controlador.setCorreo(auxCorreo);
+                controlador.init();
+            };
+    }
+
+    private ActionListener accionEditarPerfil3(){
+            return (ActionEvent ae) -> {
+                vista.dispose();
+                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
+                fEditarPerfiles.indexPerfilAux = 2;
+                CtrlEditarPerfiles cEditarPerfiles = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
+                cEditarPerfiles.setCorreo(auxCorreo);
+                cEditarPerfiles.init();
+            };
+    }
+
+    private ActionListener accionEditarPerfil4(){
+            return (ActionEvent ae) -> {
+                vista.dispose();
+                FrmEditarPerfiles fEditarPerfiles = new FrmEditarPerfiles();
+                fEditarPerfiles.indexPerfilAux = 3;
+                CtrlEditarPerfiles controlador = new CtrlEditarPerfiles(fEditarPerfiles, modelo);
+                controlador.setCorreo(auxCorreo);
+                controlador.init();
+            };
+    }
+
+    private ActionListener accionAccederPerfil1(){
+            return (ActionEvent ae) -> {
+                try {
+                    try {
+                        Cuenta c = modelo.cuentaActiva(auxCorreo); // Se obtiene la cuenta activa
+
+                        if (c.debeCobrar()) { // Si la cuenta debe cobrar
+                            if (!c.cobrar()) { // Si no se puede cobrar
+                                JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
+                                return; // Se muestra un mensaje y se termina la ejecución
+                            }
+                        }
+                    } catch (CustomException ex) {
+                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    modelo.cuentaActiva(auxCorreo).getPerfil(0).setActivo(true);
+                    vista.dispose();
+                    FrmCatalogo fCatalogo = new FrmCatalogo();
+                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
+                    ctrlCatalogo.setCorreo(auxCorreo);
+                    ctrlCatalogo.init();
+                } catch (CustomException e) {
+                    JOptionPane.showMessageDialog(vista, e.getMessage());
+                }
+            };
+    }
+
+    private ActionListener accionAccederPerfil2(){
+            return (ActionEvent ae) -> {
+                try {
+
+                    Cuenta c = null;
+
+                    try {
+                        c = modelo.cuentaActiva(auxCorreo);
+                    } catch (CustomException ex) {
+                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    if (c.debeCobrar()) {
+                        if (!c.cobrar()) {
+                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
+                            return;
+                        }
+                    }
+
+                    modelo.cuentaActiva(auxCorreo).getPerfil(1).setActivo(true);
+                    vista.dispose();
+                    FrmCatalogo fCatalogo = new FrmCatalogo();
+                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
+                    ctrlCatalogo.setCorreo(auxCorreo);
+                    ctrlCatalogo.init();
+                } catch (CustomException e) {
+                    JOptionPane.showMessageDialog(vista, e.getMessage());
+                }
+            };
+    }
+
+    private ActionListener accionAccederPerfil3(){
+            return (ActionEvent ae) -> {
+                try {
+
+                    Cuenta c = null;
+
+                    try {
+                        c = modelo.cuentaActiva(auxCorreo);
+                    } catch (CustomException ex) {
+                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    if (c.debeCobrar()) {
+                        if (!c.cobrar()) {
+                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
+                            return;
+                        }
+                    }
+
+                    modelo.cuentaActiva(auxCorreo).getPerfil(3).setActivo(true);
+                    vista.dispose();
+                    FrmCatalogo fCatalogo = new FrmCatalogo();
+                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
+                    ctrlCatalogo.setCorreo(auxCorreo);
+                    ctrlCatalogo.init();
+                } catch (CustomException e) {
+                    JOptionPane.showMessageDialog(vista, e.getMessage());
+                }
+            };
+    }
+
+    private ActionListener accionAccederPerfil4(){
+            return (ActionEvent ae) -> {
+                try {
+
+                    Cuenta c = null;
+
+                    try {
+                        c = modelo.cuentaActiva(auxCorreo);
+                    } catch (CustomException ex) {
+                        Logger.getLogger(CtrlPerfiles.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    if (c.debeCobrar()) {
+                        if (!c.cobrar()) {
+                            JOptionPane.showMessageDialog(vista, "No cuenta con dinero suficiente para continuar");
+                            return;
+                        }
+                    }
+
+                    modelo.cuentaActiva(auxCorreo).getPerfil(3).setActivo(true);
+                    vista.dispose();
+                    FrmCatalogo fCatalogo = new FrmCatalogo();
+                    CtrlCatalogo ctrlCatalogo = new CtrlCatalogo(modelo, fCatalogo);
+                    ctrlCatalogo.setCorreo(auxCorreo);
+                    ctrlCatalogo.init();
+                } catch (CustomException e) {
+                    JOptionPane.showMessageDialog(vista, e.getMessage());
+                }
+            };
+    }
+
+    private ActionListener accionSalir(){
+            return (ActionEvent ae) -> {
+                try {
+                    vista.dispose();
+                    Cuenta a = modelo.cuentaActiva(auxCorreo);
+                    String Correo = a.getUsuario().getCorreo();
+                    System.out.println(Correo);
+                    modelo.cuentaActiva(auxCorreo).getUsuario().disminuirDispositivo();
+                    // modelo.cambiarEstadoSesion(Correo);
+                    //GUARDANDO LOS CAMBIOS EN EL ARCHIVO//
+                    Archivo archivo = new Archivo();
+                    archivo.serializar(Archivo.archivoArregloCuentas, modelo);
+                    // // // // // // // // // // // // // // 
+
+                    FrmInicio fInicio = new FrmInicio();
+                    CtrlInicio ctrlInicio = new CtrlInicio(modelo, fInicio);
+
+                    ctrlInicio.init();
+                } catch (CustomException e) {
+                    System.out.println("EXCEPCION: " + e.getMessage());
+                }
+            };
     }
 
 }
